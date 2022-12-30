7c6c19938be2a6c3b132d0d099dc4c966da7d7ff
“scratchytube” <bpsahota@gmail.com>
2022-12-30 10:13:17 -0500

    updated

--- DataStructures/linked-list.js
+++ DataStructures/linked-list.js
@@ -35,82 +35,102 @@ class SinglyLinkedList {
     this.tail.next = null;
     this.length--;
     if (this.length === 0) {
-      this.tail = null;
       this.head = null;
+      this.tail = null;
     }
-
     return current;
   }
   shift() {
-    if (!this.head) {
-      return undefined;
-    } else {
-      var currentHead = this.head;
-    }
+    if (!this.head) return undefined;
+    var currentHead = this.head;
     this.head = currentHead.next;
     this.length--;
     if (this.length === 0) {
       this.tail = null;
-      this.head = null;
     }
     return currentHead;
   }
-
   unshift(val) {
     var newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
+      this.tail = this.head;
     }
-
+    newNode.next = this.head;
+    this.head = newNode;
     this.length++;
     return this;
   }
   get(index) {
-    if (index < 0 || index >= this.length) {
-      return null;
-    }
-    var count = 0;
+    if (index < 0 || index >= this.length) return null;
+    var counter = 0;
     var current = this.head;
-    while (count !== index) {
+    while (counter !== index) {
       current = current.next;
-      count++;
+      counter++;
     }
     return current;
   }
   set(index, val) {
-    var nodeToUpdate = this.get(index);
-    if (nodeToUpdate) {
-      nodeToUpdate.val = val;
+    var foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
   insert(index, val) {
-    if (index > this.length || index < 0) return false;
+    if (index < 0 || index > this.length) return false;
     if (index === this.length) return !!this.push(val);
     if (index === 0) return !!this.unshift(val);
 
     var newNode = new Node(val);
-    var prevNode = this.get(index - 1);
-    var temp = prevNode.next;
-    prevNode.next = newNode;
+    var prev = this.get(index - 1);
+    var temp = prev.next;
+    prev.next = newNode;
     newNode.next = temp;
     this.length++;
     return true;
   }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    var previousNode = this.get(index - 1);
+    var removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+  reverse() {
+    var node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    var next;
+    var prev = null;
+    for (var i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
+  print() {
+    var arr = [];
+    var current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
 }
 
 var list = new SinglyLinkedList();
-list.push("YooHoo");
-list.push("toodaloo!");
-list.push("!");
-list.push("<3");
-list.push(":)");
 
-
-console.log(list)
+list.push(100);
+list.push(201);
+list.push(250);
+list.push(350);
+list.push(999);
